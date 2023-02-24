460a33c70dceeb8bac2dc1e6778046f24e534ebe
macjjuni <macjjuni@gmail.com>
2023-02-24 17:02:31 +0900

    Feat: 파비콘 컴포넌트 적용 및 메타태그 작성

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Favicon from '@/components/common/Favicon'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <Favicon />
+        </Head>
 
         <body>
           <Main />
