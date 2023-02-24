be680dc6f1925ec94fdecd85d6e7dbbc1a695d74
macjjuni <macjjuni@gmail.com>
2023-02-24 17:03:13 +0900

    Chore: Nprogress/gtag lib 설치 및 Image 도메인 등록

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['img1.daumcdn.net', 'www.notion.so', 's3.us-west-2.amazonaws.com', 'images.unsplash.com', 'kku.dev'],
+  },
 }
 
 module.exports = nextConfig
