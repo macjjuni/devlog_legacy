98e24b764791af7a4de48a798a706f8308b6fcc9
macjjuni <macjjuni@gmail.com>
2023-02-24 16:59:06 +0900

    Fix: Notion 렌더 컴포넌트 오류 수정 & 스타일 작업

--- src/components/common/NotionRenderer/index.tsx
+++ src/components/common/NotionRenderer/index.tsx
@@ -7,6 +7,7 @@ import type { ExtendedRecordMap } from 'notion-types'
 import { useAppSelector } from '@/redux/hook'
 import { dark } from '@/redux/slice/theme'
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
 const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code))
 const Collection = dynamic(() => import('react-notion-x/build/third-party/collection').then((m) => m.Collection))
 const Equation = dynamic(() => import('react-notion-x/build/third-party/equation').then((m) => m.Equation))
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,6 +2,6 @@ const ease = 'ease duration-500'
 
 export const text = {
   black: `text-black dark:text-[#fff] ${ease}`,
-  normal: `text-BLG800 dark:text-BLG200 ${ease}`,
+  normal: `text-BLG800 dark:text-COLOR_0 ${ease}`,
   light: `text-BLG700 dark:text-BLG300 ${ease}`,
 }
