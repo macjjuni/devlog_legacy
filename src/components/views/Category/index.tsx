38407901519128f65cb5374c3fb18e0963907873
macjjuni <macjjuni@gmail.com>
2023-02-26 02:21:19 +0900

    Feat: 카테고리 컴포넌트 작성

--- src/components/views/Category/index.tsx
+++ src/components/views/Category/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import Link from 'next/link'
+import type { SelectPropertyResponse } from '@/notion/types'
+import { COLOR_TABLE } from '@/styles/notion.style'
 
-const index = () => {
-  return <div />
+interface IBlogCategory {
+  postCount: number
+  category: SelectPropertyResponse[] | undefined
 }
 
-export default index
+const Category = ({ postCount, category }: IBlogCategory) => {
+  console.log(category)
+
+  return (
+    <aside>
+      <ul className="flex justify-start items-center gap-4 h-[80px] overflow-auto">
+        <li className="text-BLG800 TagItem ease" style={{ borderColor: COLOR_TABLE.default, background: COLOR_TABLE.default }}>
+          <Link href="/devlog">
+            <a>전체 글</a>
+          </Link>
+        </li>
+        {category?.map((item) => (
+          <li key={item.id} className="text-BLG800 TagItem ease" style={{ borderColor: COLOR_TABLE[item.color], background: COLOR_TABLE[item.color] }}>
+            <Link href={`/category/${item.name}`}>
+              <a>{item.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default Category
---
+++ src/pages/category/[id].tsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react'
+import type { GetStaticProps, GetStaticPaths } from 'next'
+import { useRouter } from 'next/router'
+import { POSTS_PER_PAGE } from '@/notion/config'
+import { getCachedDatabaseItems } from '@/notion/utils/getCachedDatabaseItems'
+import { parseDatabaseItems } from '@/notion/utils/parseDatabaseItems'
+import { initBlogInfo } from '@/notion/notion'
+import PageHead from '@/components/common/PageHead'
+import Banner from '@/components/views/Banner'
+import PostList from '@/components/views/PostList'
+import Category from '@/components/views/Category'
+import Pagination from '@/components/views/Pagination'
+import type { IBlogData, IDevLogData } from '@/notion/types'
+
+import { checkKorean } from '@/notion/utils/checkKorean'
+
+interface ICateory {
+  data: IDevLogData[]
+  blogData: IBlogData
+}
+
+const CategoryPage = ({ data, blogData }: ICateory) => {
+  const { query } = useRouter()
+  const currentPage = query.page ? parseInt(query.page.toString(), 10) : 1
+  const [postData, setPostData] = useState(data.slice(POSTS_PER_PAGE * (currentPage - 1), POSTS_PER_PAGE * currentPage))
+  const [postCount] = useState(data.length)
+
+  useEffect(() => {
+    setPostData(data.slice(POSTS_PER_PAGE * (currentPage - 1), POSTS_PER_PAGE * currentPage))
+  }, [currentPage, data])
+  return (
+    <section className="">
+      <Banner data={blogData} />
+      <PageHead subTitle="DevLog" />
+      <Category postCount={postCount} category={blogData?.category?.options} />
+      <PostList data={postData} />
+      <Pagination current={currentPage} total={data.length} />
+    </section>
+  )
+}
+
+export const getStaticProps: GetStaticProps<ICateory> = async ({ params }) => {
+  const queryId = params?.id?.toString()
+  if (!queryId || queryId === '') return { redirect: { destination: '/', permanent: false } }
+
+  const isKorean = checkKorean(queryId)
+  const name = isKorean ? queryId : queryId.replace(/\b[a-z]/g, (char) => char.toUpperCase())
+
+  try {
+    const databaseId = process.env.NOTION_DATABASEID
+    if (!databaseId) throw new Error('DATABASE_ID is not defined')
+    const databaseItems = await getCachedDatabaseItems(databaseId, { categoryName: name })
+
+    const parsedData = parseDatabaseItems(databaseItems)
+    const blogData = await initBlogInfo(databaseId)
+
+    return {
+      props: {
+        data: parsedData,
+        blogData,
+      },
+      revalidate: 60 * 5,
+    }
+  } catch (e) {
+    console.error(e)
+    return { notFound: true }
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const databaseId = process.env.NOTION_DATABASEID
+  if (!databaseId) throw new Error('DATABASE_ID is not defined')
+  const { category } = await initBlogInfo(databaseId)
+
+  const paths = category?.options.map(({ name: cateName }) => ({
+    params: { id: cateName },
+  }))
+
+  if (!paths || paths.length === 0) throw new Error('No Categories')
+
+  return {
+    paths,
+    // 등록된 카테고리 외에 없을경우 404 예외처리
+    fallback: false,
+  }
+}
+
+export default CategoryPage
