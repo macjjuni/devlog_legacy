0118c59acbfafcac5a4557c99c4c0dfa1c90792f
macjjuni <macjjuni@gmail.com>
2023-03-01 00:31:40 +0900

    Design: 스켈레톤 UI 변경 및 배너 Img 태그 사용

--- src/components/views/Banner/components/Skeleton.tsx
+++ src/components/views/Banner/components/Skeleton.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const Skeleton = () => {
-  return (
-    <div className="relative w-full h-full bg-BLG100 dark:bg-BLG800 ease">
-      <div className="skeleton-bar absolute l-[-100px] w-[200px] h-[120%] rotate-[12deg] blur-2xl bg-BLG50 dark:bg-BLG700 ease" />
-    </div>
-  )
+  return <div className="relative animate-pulse w-full h-full bg-BLG300 dark:bg-BLG700" />
 }
 
 export default Skeleton
