662fd3afc3b7e79895a59a363b72dbf3ffe2a504
macjjuni <macjjuni@gmail.com>
2023-03-03 12:51:03 +0900

    Feat: 검색 페이지 전용 텍스트 추가

--- src/components/views/NoPost/index.tsx
+++ src/components/views/NoPost/index.tsx
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/router'
 import { text } from '@/styles/global'
 
 const NoPost = () => {
-  return <li className={`${text.light} fcc h-[200px]`}>현재 글이 없습니다.</li>
+  const { pathname, query } = useRouter()
+  const searchText = () => {
+    if (typeof query.name === 'string') return query.name
+    return ''
+  }
+  return <li className={`${text.light} fcc h-[200px]`}>{pathname.includes('search') ? `"${searchText()}" 검색 결과가 없습니다.` : '현재 글이 없습니다.'}</li>
 }
 
 export default NoPost
