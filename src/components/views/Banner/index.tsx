27523485888ac814d0fc82d63ff542c79511db95
macjjuni <macjjuni@gmail.com>
2023-03-03 13:38:31 +0900

    Feat: 배너 커버 이미지 변경

--- src/components/views/Banner/index.tsx
+++ src/components/views/Banner/index.tsx
@@ -1,4 +1,5 @@
 import { useState, memo } from 'react'
+import Image from 'next/image'
 import Skeleton from './components/Skeleton'
 import type { IBlogData } from '@/types/types'
 import { text } from '@/styles/global'
@@ -14,7 +15,7 @@ const Banner = ({ data }: { data: IBlogData }) => {
     <div className="Banner-wrap">
       <div className="Banner-top-wrap relative w-full md:h-[300px] h-[200px] rounded-[3px] overflow-hidden z-[-1] select-none">
         {!isLoad && <Skeleton />}
-        <img src={data.coverURL} alt="kku.dev Top Banner" onLoad={onLoadImage} className="absolute top-[50%] translate-y-[-40%] l-[0] w-full h-[auto] " width="1200" height="700" />
+        <Image src="/images/banner-cover.webp" alt="kku.dev Top Banner" onLoad={onLoadImage} className="absolute top-[50%] translate-y-[-40%] l-[0] w-full h-[auto] " width="1200" height="700" />
       </div>
       <div className="flex justify-between items-center flex-col gap-[8px] pt-[24px] pb-[8px]">
         <h1 className={`${text.light} flex justify-between items-center text-head_lg fwb w-full`}>
