a1b3e98c5da63f0a6c10e486f133f7f4107a9b07
macjjuni <macjjuni@gmail.com>
2023-03-29 11:11:51 +0900

    feat(comp/SearchBar): 아이콘 CSS 및 클릭 이벤트 기능 추가

--- src/components/views/SearchBar/index.tsx
+++ src/components/views/SearchBar/index.tsx
@@ -28,7 +28,10 @@ const SearchBar = () => {
 
   const onMouseClick = throttle(() => {
     if (searchRef.current === null) return
-    if (searchRef.current.value.trim() === '') return
+    if (searchRef.current.value.trim() === '') {
+      searchRef.current.focus()
+      return
+    }
     onSearch(searchRef.current?.value)
   }, 700)
 
@@ -42,13 +45,13 @@ const SearchBar = () => {
   return (
     <div className="SearchBar w-full md:w-[auto] flex justify-end items-center py-[4px] mt-[10px] md:m-0">
       <div className="SearchWrap relative">
-        <BiSearchAlt onClick={onMouseClick} fontSize={24} className={`${text.black} absolute top-[50%] left-[4px] translate-y-[-50%] text-BLG700 cursor-pointer`} />
+        <BiSearchAlt onClick={onMouseClick} fontSize={24} className={`${text.black} absolute top-[50%] left-[8px] translate-y-[-50%] text-BLG700 cursor-pointer`} />
         <input
           ref={searchRef}
           onKeyDown={onKeyEvent}
           type="text"
           maxLength={100}
-          className={`${text.black} text-md py-[5px] pl-[32px] pr-[12px] outline-none bg-BLG200 dark:bg-BLG700 w-[128px] focus:w-[180px] ease rounded-full`}
+          className={`${text.black} text-md py-[5px] pl-[36px] pr-[12px] outline-none bg-BLG200 dark:bg-BLG700 w-[128px] focus:w-[180px] ease rounded-full`}
         />
       </div>
     </div>
