c7aa71e5309a8a342a7e7fbaab3f5fa2cca1d8c0
macjjuni <macjjuni@gmail.com>
2023-04-16 18:35:23 +0900

    style: 글 목록 타이틀 폰트사이즈 수정 및 TOC sticky 속성 적용

--- src/components/views/PostList/index.tsx
+++ src/components/views/PostList/index.tsx
@@ -14,7 +14,7 @@ const PostList = ({ data }: IPostList) => {
         <li key={post.id} className="block border-b border-solid border-BG_0 py-xlg md:px-sm">
           <Link href={`/blog/${post.id}`} className="flex flex-col justify-start w-full">
             <p className="text-md text-primary fwb">{post.category?.name}</p>
-            <h2 className={`text-head_sm ${text.normal} fwb whitespace-nowrap truncate`}>{post.title}</h2>
+            <h2 className={`text-xlg md:text-head_sm ${text.normal} fwb whitespace-nowrap truncate`}>{post.title}</h2>
             <p className="text-sm text-BLG500">{post.published.substring(0, 10)}</p>
             <ul className="flex justify-start items-center gap-[1rem] mt-[10px] h-[28px]">
               {post.tags.map((tag) => (
