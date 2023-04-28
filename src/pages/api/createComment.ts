959b752ce1aa9a5bfdbd38e8482a8bb1f23043f2
macjjuni <macjjuni@gmail.com>
2023-04-28 16:30:46 +0900

    feat: 노션 댓글 작성 API 작성(주석처리)

---
+++ src/pages/api/createComment.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+// import { notion } from '@/notion/notion'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<{ data: boolean }>) => {
+  // const respense = await notion.comments.create({
+  //   parent: {
+  //     page_id: '434339f24bcf4440b1f7ab048f546801',
+  //   },
+  //   rich_text: [
+  //     {
+  //       text: {
+  //         content: 'Hello world',
+  //       },
+  //     },
+  //   ],
+  // })
+  // console.log(respense)
+  res.status(200).json({ data: true })
+}
+export default handler
