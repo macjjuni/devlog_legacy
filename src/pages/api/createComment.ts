6e2695eab8d31521afe0c64f184219d8e90ec410
macjjuni <macjjuni@gmail.com>
2023-05-01 16:49:09 +0900

    feat: 메시지 작성 유저정보 수집

--- src/pages/api/createComment.ts
+++ src/pages/api/createComment.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import requestIp from 'request-ip'
 import { notion } from '@/notion/notion'
 
 interface CreateCommentReq extends NextApiRequest {
@@ -6,25 +7,23 @@ interface CreateCommentReq extends NextApiRequest {
     message: string
   }
 }
+// 댓글용 Page ID
+const pageId = '1c71dd13821f492f8e06c6206d21d9b2'
 
 const handler = async (req: CreateCommentReq, res: NextApiResponse<{ status: boolean }>) => {
   // 잘못된 호출 핸들링
   if (req.method !== 'POST' || !req.body.message) {
     res.status(200).json({ status: false })
   }
+
+  // Get user IP
+  const userIP = requestIp.getClientIp(req) || 'unknown'
+
   // 댓글 작성
   try {
     await notion.comments.create({
-      parent: {
-        page_id: '1c71dd13821f492f8e06c6206d21d9b2',
-      },
-      rich_text: [
-        {
-          text: {
-            content: req.body.message,
-          },
-        },
-      ],
+      parent: { page_id: pageId },
+      rich_text: [{ text: { content: `${req.body.message} - ${userIP}` } }],
     })
     res.status(200).json({ status: true })
   } catch (e) {
