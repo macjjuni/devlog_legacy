53d6d4655313814069deb5b5d251f2345e141722
macjjuni <macjjuni@gmail.com>
2023-05-01 15:18:12 +0900

    feat: 로딩 스피너 추가 및 리팩토링

--- src/components/views/MessageBox/index.tsx
+++ src/components/views/MessageBox/index.tsx
@@ -1,28 +1,47 @@
-import { useRef } from 'react'
+import { useState, useRef } from 'react'
 import { toast } from 'react-toastify'
-import { sendMessage } from '@/api/message'
+import { writeMessage } from '@/api/message'
+import LottieItem from '@/components/common/LottieItem'
+import SpinnerLottie from '@/assets/lottie/spinner.json'
+
+interface IValid {
+  status: boolean
+  msg?: string
+}
+
+// Lottie Option
+const defaultOption = {
+  loop: true,
+  play: true,
+  style: { width: '25px', height: '25px' },
+}
 
 const MessageBox = () => {
-  const msgRef = useRef<HTMLTextAreaElement>(null)
-  const btnRef = useRef<HTMLButtonElement>(null)
-  const isLoad = useRef<boolean>(false)
+  const msgRef = useRef<HTMLTextAreaElement>(null) // textArea Element Ref
+  const btnRef = useRef<HTMLButtonElement>(null) // send Button Element Ref
+  const [isLoad, setLoad] = useState<boolean>(false) // loading flag
 
-  const sendMsg = async () => {
-    if (!msgRef.current || isLoad.current) return
-    isLoad.current = true
-
+  // message text validate
+  const validMessage: () => IValid = () => {
+    if (!msgRef.current || isLoad) return { status: false } // 로딩중일 때 실행방지
     const msg = msgRef.current.value.trim()
     if (msg === '') {
       msgRef.current.focus()
       toast.warn('방명록을 작성해주세요 🙏')
-      isLoad.current = false
-      return
+      return { status: false }
     }
+    return { status: true, msg }
+  }
 
+  const onSendMessage = async () => {
+    const { status, msg } = validMessage()
+
+    if (!status || !msg) return
+    setLoad(true)
     try {
-      const res = await sendMessage(msg)
+      const res = await writeMessage(msg)
       if (res.data && res.data.status) {
-        msgRef.current.value = ''
+        if (msgRef.current) msgRef.current.value = ''
         toast.success('소중한 방명록 감사합니다. 🙇‍♂️')
       } else {
         toast.error('네트워크 오류입니다. 나중에 다시 시도해주세요. 🙏')
@@ -32,23 +51,24 @@ const MessageBox = () => {
       console.error(e)
     } finally {
       btnRef.current?.blur()
-      isLoad.current = false
+      setLoad(false)
     }
   }
 
   return (
-    <div className="flex flex-col gap-2 ">
+    <div className="flex flex-col gap-2 mb-[20px]">
       <textarea
         ref={msgRef}
         name="kku-dev-message"
         id="kku-dev-message"
+        disabled={isLoad}
         maxLength={255}
         placeholder="소중한 방명록을 남겨주세요 ✉️"
         className="w-full border border-BLG600 p-md outline-0 resize-none"
       />
       <div className="flex justify-end">
-        <button ref={btnRef} type="button" onClick={sendMsg} className="fcc border border-BLG600 w-[80px] h-[34px] px-lg">
-          보내기
+        <button ref={btnRef} disabled={isLoad} type="button" onClick={onSendMessage} className="fcc border border-BLG600 w-[80px] h-[34px] px-lg">
+          {!isLoad ? '보내기' : <LottieItem defaultOption={defaultOption} animationData={SpinnerLottie} />}
         </button>
       </div>
     </div>
