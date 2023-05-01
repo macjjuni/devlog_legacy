d66d32680f7c305024c1388e274fa2b7ff6d2637
macjjuni <macjjuni@gmail.com>
2023-05-01 18:10:30 +0900

    style: 윈도우 OS 스타일 맞춤

--- src/components/views/MessageBox/index.tsx
+++ src/components/views/MessageBox/index.tsx
@@ -65,19 +65,19 @@ const MessageBox = () => {
         disabled={isLoad}
         maxLength={255}
         placeholder="소중한 방명록을 남겨주세요 ✉️"
-        className="w-full border border-BLG600 dark:bg-BLG800 dark:text-BLG300 p-md outline-0 duration-300 resize-none"
+        className="basic w-full border border-BLG600 dark:bg-BLG800 dark:text-BLG300 p-md outline-0 duration-300 resize-none"
       />
       <div className="flex justify-between items-center">
         <div className="fcc">
-          <BsInfoCircle className="mr-2 mt-[1px] dark:text-BLG100 durtaion-300" />
-          <span className="text-[12px] dark:text-BLG300 leading-[1.2]">
+          <BsInfoCircle className="mr-2 dark:text-BLG100 durtaion-300" />
+          <span className="relative top-[1px] text-[12px] dark:text-BLG300 leading-[1.2]">
             메시지는 주인장만 볼 수 있있습니다.
             {/* <br />
             악성 봇 차단을 위해 IP 주소를 수집하고 있습니다. */}
           </span>
         </div>
-        <button ref={btnRef} disabled={isLoad} type="button" onClick={onSendMessage} className="fcc w-[80px] h-[34px] px-lg border border-BLG600 dark:text-BLG300 duration-300">
-          {!isLoad ? '보내기' : <LottieItem defaultOption={defaultOption} animationData={SpinnerLottie} />}
+        <button ref={btnRef} disabled={isLoad} type="button" onClick={onSendMessage} className="fcc w-[100px] h-[34px] px-lg border border-BLG600 dark:text-BLG300 duration-300">
+          {!isLoad ? <span className="relative top-[2px]">보내기</span> : <LottieItem defaultOption={defaultOption} animationData={SpinnerLottie} />}
         </button>
       </div>
     </div>
