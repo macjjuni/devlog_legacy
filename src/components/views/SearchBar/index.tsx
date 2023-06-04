0e16d983fcb87ab886ebc1d9241aec2928cbf58d
macjjuni <macjjuni@gmail.com>
2023-06-05 03:11:45 +0900

    Style: 검색바 스타일 수정

--- src/components/views/SearchBar/index.tsx
+++ src/components/views/SearchBar/index.tsx
@@ -43,7 +43,7 @@ const SearchBar = () => {
   }, [])
 
   return (
-    <div className="SearchBar w-full md:w-[auto] flex justify-end items-center py-[4px] mt-[10px] md:m-0">
+    <div className="SearchBar w-full md:w-[auto] flex justify-end items-center mt-[10px] md:m-0">
       <div className="SearchWrap relative">
         <BiSearchAlt onClick={onMouseClick} fontSize={24} className={`${text.black} absolute top-[50%] left-[8px] translate-y-[-50%] text-BLG700 cursor-pointer`} />
         <input
@@ -51,7 +51,7 @@ const SearchBar = () => {
           onKeyDown={onKeyEvent}
           type="text"
           maxLength={100}
-          className={`${text.black} text-md py-[5px] pl-[36px] pr-[12px] outline-none bg-BLG100 dark:bg-BLG800 w-[128px] focus:w-[180px] ease rounded-full basic`}
+          className={`${text.black} text-md py-[8px] pl-[36px] pr-[12px] outline-none bg-BLG100 dark:bg-BLG800 w-[128px] focus:w-[220px] ease rounded-full basic`}
         />
       </div>
     </div>
