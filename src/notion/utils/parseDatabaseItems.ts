4062865cceaef634f67336d8f758025df4a1c63f
macjjuni <macjjuni@gmail.com>
2023-09-03 20:32:14 +0900

    Fix: 포스트 썸네일 타입오류 수정

--- src/notion/utils/parseDatabaseItems.ts
+++ src/notion/utils/parseDatabaseItems.ts
@@ -2,6 +2,7 @@ import { getDatabaseItems } from '../notion'
 import { IPosts } from '@/types/types'
 
 const siteURL = process.env.SITE_URL || 'http://kku.dev'
+const defaultThumb = `${siteURL}/image/post-cover.webp`
 
 // 노션 API로 받은 데이터 가공
 export const parseDatabaseItems = (databaseItems: Awaited<ReturnType<typeof getDatabaseItems>>) =>
@@ -12,7 +13,11 @@ export const parseDatabaseItems = (databaseItems: Awaited<ReturnType<typeof getD
     const { 카테고리, 작성일, 이름, 태그 } = item.properties
 
     // 블로그 목록 데이터 가공
-    const cover = item.cover?.type === 'external' ? item.cover.external.url : item.cover?.file ? item.cover.file.url : `${siteURL}/image/post-cover.webp`
+
+    let cover = ''
+    if (item.cover === null) cover = defaultThumb
+    else cover = item.cover?.type === 'external' ? item.cover.external.url : item.cover?.file ? item?.cover?.file.url : defaultThumb
+
     const title = 이름?.type === 'title' ? 이름.title[0].plain_text : ''
     const published = 작성일?.type === 'date' ? (작성일.date?.start ? 작성일.date.start : '') : '' || ''
     const category = 카테고리?.type === 'select' ? 카테고리?.select : null
